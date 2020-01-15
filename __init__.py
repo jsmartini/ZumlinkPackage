ecbe04fbdcb2506fed21c0ee68c41fa2ce985aec
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2020-01-14 21:53:44 -0600

    next

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from pkg.debug import mode
+from pkg.debug import Debug
+from pkg.radio import Zumlink
+from pkg.radio import generate_config
+
+if __name__ == "__main__":
+    print("Zumlink IOT py3 lib")
+    print("Have fun freshmen")
